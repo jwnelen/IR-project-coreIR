03969a38c47be8e4506b4bf833f63455a6a1f195
Francis Behnen <francis.behnen@gmail.com>
2022-02-24 13:41:01 +0100

    Add BM25 scoring

---
+++ score.py
@@ -0,0 +1,21 @@
+
+from math import log
+
+k1 = 1.2
+k2 = 100
+b = 0.75
+R = 0.0
+
+
+# This returns the score for a single word, combination of multiple words is left to the caller
+# ni: Number of documents containing the term
+# N: number of documents in the corpus
+# Complete other terms...
+def score_BM25(ri, ni, N, fi, qfi, dl, avdl):
+    K = k1 * ((1-b) + b * dl / avdl)
+
+    part1 = log((ri + 0.5)/(R - ri + 0.5)/((ni - ri + 0.5)/(N - ni - R + ri + 0.5)))
+    constants = ((k1 + 1)*fi)/(K + fi)
+    part3 = ((k2 + 1)*qfi)/(k2 + qfi)
+
+    return part1 * constants * part3
