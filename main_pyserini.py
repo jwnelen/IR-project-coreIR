94fcb7ab058c23380673a60307b3519071eb0da7
Francis Behnen <francis.behnen@gmail.com>
2022-02-27 00:02:58 +0100

    Fix module not found error
    https://github.com/castorini/pyserini/issues/1025

--- main_pyserini.py
+++ main_pyserini.py
@@ -1,9 +1,7 @@
-from pyserini.search.lucene import LuceneSearcher
+from pyserini.search import SimpleSearcher
 
+searcher = SimpleSearcher.from_prebuilt_index('msmarco-passage')
+hits = searcher.search('what is a lobster roll?')
 
-if __name__ == '__main__':
-    searcher = LuceneSearcher.from_prebuilt_index('msmarco-v1-passage')
-    hits = searcher.search('what is a lobster roll?')
-
-    for i in range(0, 10):
-        print(f'{i+1:2} {hits[i].docid:7} {hits[i].score:.5f}')
+for i in range(0, 10):
+    print(f'{i+1:2} {hits[i].docid:7} {hits[i].score:.5f}')
