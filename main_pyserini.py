8a2b266cc8248681932a0b10b71ae507c391691d
Jeroen Nelen <jwnelen@gmail.com>
2022-03-03 11:23:54 +0100

    Now training file can be made. Also added the ranklib jar for easiness

--- main_pyserini.py
+++ main_pyserini.py
@@ -5,27 +5,23 @@ import json
 from pyserini.index import IndexReader
 from pyserini.analysis import Analyzer, get_lucene_analyzer
 
-from progressbar import ProgressBar, Percentage, Bar
 
-
-def do_search():
+def learn_to_rank():
     index_reader = IndexReader('indexes/sample_collection_jsonl')
-    d = []
-
     qrels = pd.read_csv("data/qrels.dev.tsv", sep='\t', header=None)
     queries = pd.read_csv("data/queries/queries.dev.tsv", sep='\t', names=['qid', 'query'], header=None)
 
+    training_data_list = list()
     # Default analyzer for English uses the Porter stemmer:
     analyzer = Analyzer(get_lucene_analyzer())
 
     print(index_reader.stats())
-    print(qrels.head())
-    print(queries.head())
-    ep = 0.00001
 
+    # Number of documents
     N = index_reader.stats().get("documents")
+    epsilon = 0.00001
 
-    for index, row in qrels.iterrows():
+    for idx, row in qrels.iterrows():
         qid = row[0]
         docid = row[2]
         query_frame = queries[queries["qid"] == qid]
@@ -34,12 +30,10 @@ def do_search():
         document_vector = index_reader.get_document_vector(str(docid))
 
         doc = index_reader.doc(str(docid))
-        # print('contents', json.loads(doc.raw()).get("contents"))
-        # print('vector', document_vector)
         doc_length = len(json.loads(doc.raw()).get("contents").split())
 
+        # Splitting it into tokens
         tokens = analyzer.analyze(query)
-        # print('tokens', tokens)
 
         tf_idfs = list()
         for term in tokens:
@@ -49,7 +43,7 @@ def do_search():
                 # document frequency, collection frequency
                 df, nk = index_reader.get_term_counts(term,
                                                       analyzer=analyzer.analyzer)
-                idf = math.log(N / (nk + ep))
+                idf = math.log(N / (nk + epsilon))
 
                 frequency_term_in_document = document_vector.get(term, 0)
                 tf = frequency_term_in_document / doc_length
@@ -60,16 +54,17 @@ def do_search():
         if not query is None:
             score = index_reader.compute_query_document_score(str(docid), str(query))
             query_length = len(query.split())
-            d.append([1, qid, docid, score, sum(tf_idfs), doc_length, query_length])
+            training_data_list.append([1, qid, docid, score, sum(tf_idfs), doc_length, query_length])
 
-    results = pd.DataFrame(d, columns=["relevance", "qid", "docid", "BM25", "TFIDF", "doc length", "query length"])
+    results = pd.DataFrame(training_data_list,
+                           columns=["relevance", "qid", "docid", "BM25", "TFIDF", "doc length", "query length"])
+
     # set the max columns to none
     pd.set_option('display.max_columns', None)
-
     print(results.head())
     print(results.describe())
 
-    results.to_csv('results')
+    results.to_csv('results.csv')
 
 
 # Blatantly copied from https://www.geeksforgeeks.org/python-tsv-conversion-to-json/
@@ -92,15 +87,26 @@ def tsv2json(input_file, output_file):
             output_file.write(json.dumps(arr, indent=4))
 
 
-if __name__ == '__main__':
-    # Do not do this, this is way too expensive
-    # # Convert collection into JSON for Pyserini index builder
-    # datafolder = 'data/collectionandqueries/'
-    # collection_file = datafolder + 'collection.tsv'
-    # collection_file_json = datafolder + 'collection.json'
-    # if not path.exists(collection_file_json):
-    #     # tsv2json(collection_file, collection_file_json)
-    #     df = pd.read_csv(collection_file, sep='\t')
-    #     df.to_json(collection_file_json)
+def toPrintString(qid, bm25, tdidf, doc_length, query_length):
+    # 3 qid:1 1:1 2:1 3:0 4:0.2 5:0
+    return f"1 qid:{qid} 1:{bm25} 2:{tdidf} 3:{doc_length} 4:{query_length}"
 
-    do_search()
+
+if __name__ == '__main__':
+    # learn_to_rank()
+
+    # For this, first the learn to rank should be called
+    training_data = pd.read_csv("results.csv")
+    print(training_data)
+
+    with open('training.txt', 'w') as f:
+        for index, row in training_data.iterrows():
+            qid = row[2]
+            bm25 = row[4]
+            tfidf = row[5]
+            doc_length = row[6]
+            query_length = row[7]
+            f.write(toPrintString(qid, bm25, tfidf, doc_length, query_length))
+            f.write("\n")
+
+    f.close()
